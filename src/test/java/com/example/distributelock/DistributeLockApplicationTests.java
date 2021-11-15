84c15d7c1ab7b9116fbf7e6dd252c876ffd3f1db
zhangjinyu <zhangjnyu@qiaojizixun.com>
2021-11-15 15:40:20 +0800

    1

---
+++ src/test/java/com/example/distributelock/DistributeLockApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.distributelock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DistributeLockApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void a() {
+        System.out.println("888888888888888888");
+
+    }
+}
